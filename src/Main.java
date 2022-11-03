5acc0403945b95115cbcb53e132ef0a8cbdc99e7
kymdennis3 <kymdennis3@gmail.com>
2022-11-03 08:40:06 +0300

    added get a single list feature

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<ArrayList<String>> shopping = new ArrayList();
+
+        ArrayList<String> groceries = new ArrayList();
+
+        groceries.add("tomatoes");
+        groceries.add("Onions");
+        groceries.add("Garlic");
+
+        ArrayList<String> food = new ArrayList();
+
+        food.add("Dengu");
+        food.add("Bread");
+        food.add("Flour");
+
+        ArrayList<String> drinks = new ArrayList();
+
+        drinks.add("Soda");
+        drinks.add("Beer");
+        drinks.add("Milk");
+
+        shopping.add(groceries);
+        shopping.add(food);
+        shopping.add(drinks);
+
+        System.out.println(shopping.get(0));
     }
 }
